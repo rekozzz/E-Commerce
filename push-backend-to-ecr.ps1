b862ce2f60598eafe261c9dbc59c61fa0737c4e3
Patrick <rekoshehata@gmail.com>
2026-05-12 02:56:06 +0300

    peekabo

--- push-backend-to-ecr.ps1
+++ push-backend-to-ecr.ps1
@@ -3,8 +3,8 @@ $AWS_ACCOUNT_ID="657588917420"
 $AWS_REGION="us-east-1"  # Change if your region is different
 $REPO_NAME="nexshop-backend"
 
-Write-Host "1. Creating ECR Repository (if it doesn't exist)..."
-aws ecr create-repository --repository-name $REPO_NAME --region $AWS_REGION 2>$null
+# Write-Host "1. Creating ECR Repository (if it doesn't exist)..."
+# aws ecr create-repository --repository-name $REPO_NAME --region $AWS_REGION 2>$null
 
 Write-Host "2. Logging into AWS ECR..."
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com"
